da1e15c5f3caf3e033f3d8e5fe018d9bc3a6f76b
Subrata Ghosh Dastidar <github.subrata@gmail.com>
2023-03-15 01:00:24 +0530

    modify main.py file

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": 'All Posts is here'}
+
+
+@app.post("/createposts")
+def create_post(new_post: Post):
+    print(new_post)
+    return {"data": new_post}
